1f7e04d0ce93f2dc9865e2b221f2671cf97416c7
Sato <mijinco.ku@gmail.com>
2023-04-14 12:01:16 +0900

    a

---
+++ config.js
@@ -0,0 +1,8 @@
+var spot = [
+    { src: 'assets/michikusa.png', scale: '5 5 1', latitude: 24.34031586062179, longitude: 124.15826261612321 },
+    { src: 'assets/klatch.png', scale: '5 5 1', latitude: 24.340537303621755, longitude: 124.15833679907587 },
+    { src: 'assets/avance.png', scale: '6 6 1', latitude: 24.339693682834817, longitude: 124.15870952535485 },
+    { src: 'assets/cloudnine.png', scale: '6 6 1', latitude: 24.34035350949832, longitude: 124.1582387976726 },
+    { src: 'assets/cotton.png', scale: '6 6 1', latitude: 24.340054755073425, longitude: 124.15845538605058 },
+    { src: 'assets/comecome.png', scale: '6 6 1', latitude: 24.340886042637198, longitude: 124.15771680158518 },
+    { src: 'assets/gaguya.png', scale: '6 6 1', latitude: 24.3397844072719, longitude: 124.15865000000001 }]
